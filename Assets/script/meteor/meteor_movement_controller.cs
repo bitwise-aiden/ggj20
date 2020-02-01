5b84325baaba042df3b1beb7d1b8ab906804dcb0
Aiden Storey <git@storey.dev>
2020-02-01 17:48:52 -0500

    Add meteor state and movement controllers

---
+++ Assets/script/meteor/meteor_movement_controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteor_movement_controller : MonoBehaviour
+{
+    Dictionary<meteor_direction, Vector3> meteor_movement_offset = new Dictionary<meteor_direction, Vector3>()
+    {
+        { meteor_direction.left, new Vector3(-1.0f, 0.0f) },
+        { meteor_direction.right, new Vector3(1.0f, 0.0f) },
+    };
+
+    Dictionary<meteor_state, float> meteor_speed_modifier = new Dictionary<meteor_state, float>()
+    {
+        { meteor_state.idle, 0.0f },
+        { meteor_state.move, 0.05f },
+        { meteor_state.drop, 0.0f },
+    };
+
+    Vector3 _movement_offset = Vector3.zero;
+    float _speed_modifier = 0.0f;
+
+    public float speed = 1.0f;
+
+
+    void Update()
+    {
+        this.process_movement();
+    }
+
+    void process_movement()
+    {
+        var offset = this._movement_offset * (this._speed_modifier * this.speed);
+        this.transform.position = game_border_controller.instance.keep_in_bounds(this.transform.position + offset);
+    }
+
+    public void change_direction(meteor_direction direction)
+    {
+        this._movement_offset = this.meteor_movement_offset[direction];
+    }
+
+    public void change_state(meteor_state state)
+    {
+        this._speed_modifier = this.meteor_speed_modifier[state];
+    }
+}
---
+++ Assets/script/meteor/meteor_state_controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum meteor_direction { left, right };
+public enum meteor_state { idle, move, drop };
+
+public class meteor_state_controller : MonoBehaviour
+{
+    meteor_movement_controller _movement_controller;
+
+    meteor_direction _direction = meteor_direction.right;
+    meteor_state _state = meteor_state.idle;
+
+    void Start()
+    {
+        this._movement_controller = this.GetComponent<meteor_movement_controller>();
+        this._movement_controller.change_state(this._state);
+    }
+
+    public meteor_state state
+    {
+        get
+        {
+            return this._state;
+        }
+
+        set
+        {
+            if (value != this._state)
+            {
+                this._state = value;
+
+                this._movement_controller.change_state(this._state);
+            }
+        }
+    }
+
+    public meteor_direction direction {
+        get
+        {
+            return this._direction;
+        }
+
+        set
+        {
+            if (value != this._direction)
+            {
+                this._direction = value;
+
+                this._movement_controller.change_direction(this._direction);
+            }
+        }
+    }
+}
