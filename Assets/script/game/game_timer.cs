4cbb0cf68b3601fc8d7f3b6c5f80c80aa381ba97
Racheal Herlihy <racheal.herlihy@shopify.com>
2020-02-02 10:34:53 -0500

    added game timer and end scene

---
+++ Assets/script/game/game_timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class game_timer : MonoBehaviour
+{
+    public int timeRemaining = 10; //Seconds Overall
+    public Text countdown; //UI Text Object
+    void Start()
+    {
+        StartCoroutine("DropTime");
+        Time.timeScale = 1; //Just making sure that the timeScale is right
+    }
+    void Update()
+    {
+        countdown.text = ("Time Left: " + timeRemaining); //Showing the Score on the Canvas
+    }
+    ////Simple Coroutine
+    IEnumerator DropTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeRemaining--;
+
+            if (timeRemaining < 0)
+            {
+                SceneManager.LoadScene("end_scene");
+            }
+        }
+    }
+}
