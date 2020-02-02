972b4059001649447fdd884332aa87ca269fa857
Aiden Storey <git@storey.dev>
2020-02-02 13:23:27 -0500

    Remove unneeded functions from test controller

--- Assets/script/test_controller.cs
+++ Assets/script/test_controller.cs
@@ -1,73 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class test_controller : MonoBehaviour
 {
-
-    void Start()
-    {
-        this.add_ai();
-        this.randomize_dinos_speed();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        // this.change_animation();
-        // this.control_dinos();
         this.control_meteors();
-    }
-
-    void add_ai()
-    {
-        foreach(var dino_controller in FindObjectsOfType<dino_state_controller>())
-        {
-            dino_controller.gameObject.AddComponent<dino_ai_controller>();
-            dino_controller.facing_direction = (int)Mathf.Round(Random.value) == 0 ? dino_facing_direction.left : dino_facing_direction.right;
-        }
-    }
-
-    void control_dinos()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            foreach(var dino_controller in FindObjectsOfType<dino_state_controller>())
-            {
-                dino_controller.facing_direction = dino_facing_direction.left;
-                dino_controller.state = dino_state.run;
-            }
-        }
-
-        if (Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            foreach(var dino_controller in FindObjectsOfType<dino_state_controller>())
-            {
-                dino_controller.state = dino_state.idle;
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            foreach(var dino_controller in FindObjectsOfType<dino_state_controller>())
-            {
-                dino_controller.facing_direction = dino_facing_direction.right;
-                dino_controller.state = dino_state.walk;
-            }
-        }
-
-        if (Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            foreach(var dino_controller in FindObjectsOfType<dino_state_controller>())
-            {
-                dino_controller.state = dino_state.idle;
-            }
-        }
     }
 
     void control_meteors()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             foreach(var meteor_controller in FindObjectsOfType<meteor_state_controller>())
             {
@@ -76,7 +19,7 @@ public class test_controller : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             foreach(var meteor_controller in FindObjectsOfType<meteor_state_controller>())
             {
@@ -99,49 +42,6 @@ public class test_controller : MonoBehaviour
             {
                 meteor_controller.state = meteor_state.drop;
             }
-        }
-    }
-
-    void change_animation()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            foreach(var dino_controller in FindObjectsOfType<dino_animation_controller>())
-            {
-                dino_controller.change_state(dino_state.idle);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            foreach(var dino_controller in FindObjectsOfType<dino_animation_controller>())
-            {
-                dino_controller.change_state(dino_state.walk);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            foreach(var dino_controller in FindObjectsOfType<dino_animation_controller>())
-            {
-                dino_controller.change_state(dino_state.run);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            foreach(var dino_controller in FindObjectsOfType<dino_animation_controller>())
-            {
-                dino_controller.change_state(dino_state.hurt);
-            }
-        }
-    }
-
-    void randomize_dinos_speed()
-    {
-        foreach(var dino_controller in FindObjectsOfType<dino_movement_controller>())
-        {
-            dino_controller.speed = Random.value + 0.5f;
         }
     }
 }
