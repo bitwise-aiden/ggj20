f77ab7f607f8d5a0792d80a0d73be1cb8547cdc9
Aiden Storey <git@storey.dev>
2020-02-02 09:09:46 -0500

    Increase the shake, fix scene for extra movement, add state check

--- Assets/script/meteor/meteor_drop_controller.cs
+++ Assets/script/meteor/meteor_drop_controller.cs
@@ -17,17 +17,17 @@ public class meteor_drop_controller : MonoBehaviour
 
     public void change_state(meteor_state state)
     {
-        if(state == meteor_state.drop)
+        if(state == meteor_state.drop && !state_manager.processing)
         {
             var position_a = this._meteor_object.transform.position;
-            var position_b = position_a + new Vector3(0.0f, -7.20f);
+            var position_b = position_a + new Vector3(0.0f, -7.60f);
             var position_c = position_b + new Vector3(0.0f, 1.0f);
             var position_d = position_c + new Vector3(0.0f, -3.5f);
             var position_e = position_a + new Vector3(0.0f, 2.0f);
 
             state_manager.add_queue(
                 new tween(this._meteor_object, position_a, position_b, 0.5f),
-                new screen_shake(0.5f, 0.1f),
+                new screen_shake(0.5f, 0.3f),
                 new tween(this._meteor_object, position_b, position_c, 0.1f),
                 new tween(this._meteor_object, position_c, position_d, 0.35f),
                 new tween(this._meteor_object, position_e, position_a, 0.6f));
